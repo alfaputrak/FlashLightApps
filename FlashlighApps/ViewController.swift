a1f36ee38970b62ba0900e47379f4925f1c2a588
alfaputrak <alfa.putrakurnia@gmail.com>
2019-07-14 12:57:15 +0700

    Initial Commit

---
+++ FlashlighApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashlighApps
+//
+//  Created by Alfa Putra on 14/07/19.
+//  Copyright © 2019 Alfa Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
